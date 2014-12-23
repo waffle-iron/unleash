1db7243c69c4aceef070b3f449287c6b9c04adf1
Wojtek Zając <wojtek@wojtekzajac.com>
2014-12-23 21:00:58 +0100

    Avoid transclusion in directives

--- app/scripts/directives/card.js
+++ app/scripts/directives/card.js
@@ -2,17 +2,22 @@
 /* global $ */
 
 angular.module('unleashApp').directive('unleashCard', function() {
+  var linkFn = function(scope, element, attrs) {
+    var card = angular.element(element);
+
+    $(card).not('.page-account .card').on('click', function() {
+      document.body.classList.toggle('has-menu');
+    });
+  };
+
   return {
     templateUrl: 'views/partials/card.html',
-    scope: true,
+    scope: {
+      type: '@',
+      level: '@',
+      task: '@'
+    },
     replace: true,
-    transclude: true,
-    link: function(scope, element) {
-      var card = angular.element(element);
-
-      $(card).not('.page-account .card').on('click', function() {
-        document.body.classList.toggle('has-menu');
-      });
-    }
+    link: linkFn
   };
 });
