0d2cc6c6bd96ab1461b843e071d045c927f0ad56
Wojtek Zając <wojtek@wojtekzajac.com>
2014-12-29 12:45:25 +0100

    Propagate card details in sidebar

--- app/scripts/controllers/paths.js
+++ app/scripts/controllers/paths.js
@@ -35,27 +35,4 @@ angular.module('unleashApp')
       // No users found!
       $scope.users.notfound = true;
     });
-
-    // synchronize a read-only, synchronized array of messages, limit to most recent 10
-    $scope.messages = fbutil.syncArray('messages');
-
-    // display any errors
-    $scope.messages.$loaded().catch(alert);
-
-    // provide a method for adding a message
-    $scope.addMessage = function(newMessage) {
-      if( newMessage ) {
-        // push a message to the end of the array
-        $scope.messages.$add({text: newMessage})
-          // display any errors
-          .catch(alert);
-      }
-    };
-
-    function alert(msg) {
-      $scope.err = msg;
-      $timeout(function() {
-        $scope.err = null;
-      }, 5000);
-    }
   });
--- app/scripts/directives/cardDetails.js
+++ app/scripts/directives/cardDetails.js
@@ -1,19 +1,42 @@
 'use strict';
-/* global $ */
 
 angular.module('unleashApp').directive('unleashCardDetails', function() {
-  var ctrlFn = function($window, $scope, FBURL, $firebase, $element) {
-    var ref = new $window.Firebase(FBURL);
+  var ctrlFn = function($window, $scope, FBURL, $firebase, $timeout) {
+    var ref = new $window.Firebase(FBURL).child('users').child($scope.user).child('cards').child($scope.id);
 
-    $scope.getCard = function (user, id) {
-      var sync = $firebase(ref.child('users').child(user).child('cards').child(id));
+    $scope.getCard = function () {
+      var sync = $firebase(ref);
 
       $scope.card = sync.$asObject();
     };
+
+    $scope.close = function () {
+      angular.element(document.body).removeClass('has-menu');
+    };
+
+    // synchronize a read-only, synchronized array of messages, limit to most recent 10
+    $scope.messages = $firebase(ref.child('comments'));
+
+    // provide a method for adding a message
+    $scope.addMessage = function(newMessage) {
+      if( newMessage ) {
+        // push a message to the end of the array
+        $scope.messages.$add({text: newMessage})
+          // display any errors
+          .catch(alert);
+      }
+    };
+
+    function alert(msg) {
+      $scope.err = msg;
+      $timeout(function() {
+        $scope.err = null;
+      }, 5000);
+    }
   };
 
-  var linkFn = function(scope, iElement, iAttrs, ctrl) {
-    scope.getCard(iAttrs.user, iAttrs.id);
+  var linkFn = function(scope) {
+    scope.getCard();
   };
 
   return {
