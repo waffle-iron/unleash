d0e696daa41d3ac6f290f9e1c1df4dd4cbbf77da
karolsojko <karolsojko@gmail.com>
2016-01-18 20:06:33 +0100

    Moved grunt build into travis.yml file

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,6 @@
 set -ev
 
 TAG=$1
-if [ "$TRAVIS_BRANCH" == 'develop' ]; then
-    export UNLEASH_ENV='staging'
-else
-    export UNLEASH_ENV='production'
-fi
-grunt build
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t xteam/unleash:$TAG .
 docker push xteam/unleash
