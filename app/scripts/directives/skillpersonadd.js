2fa9c0891e90e6d2ef73c495edf1e1d5ad6108fd
karolsojko <karolsojko@gmail.com>
2016-03-09 15:30:27 +0100

    Adding users from a dynamic select

--- app/scripts/directives/skillpersonadd.js
+++ app/scripts/directives/skillpersonadd.js
@@ -6,19 +6,32 @@
  * # unleashSkillPersonAdd
  */
 angular.module('unleashApp')
-  .directive('unleashSkillPersonAdd', function (skillService, userService) {
+  .directive('unleashSkillPersonAdd', function ($rootScope, skillService, userService) {
+
+    var usersDiff = function (allUsers, addedUsers) {
+      return allUsers.filter(function (user) {
+        for (var i in addedUsers) {
+          if (addedUsers[i].username === user.username) {
+            return null;
+          }
+        }
+        return user;
+      });
+    };
 
     var update = function(scope, skill, username) {
       skillService.addUserToSkill(skill, username);
       userService.findByUsernames([username]).then(function (users) {
         scope.users.push(users.shift());
+        scope.user = null;
+        scope.allUsers = usersDiff(scope.allUsers, scope.users);
       });
-      scope.username = '';
     };
 
     return {
       templateUrl: 'views/partials/skillPersonAdd.html',
       link: function postLink(scope) {
+        scope.allUsers = usersDiff($rootScope.allUsers, scope.users);
         scope.update = function(skill, username) {
           update(scope, skill, username);
         };
