6f74e84593dc8d376c7eafe3477df1b96ee6b237
karolsojko <karolsojko@gmail.com>
2016-07-21 14:24:52 +0200

    Remove unused object.assign

--- app/utils/assign.js
+++
@@ -1,7 +0,0 @@
-const assign = Object.assign || require('object.assign'); // Polyfill maybe needed for browser support
-
-function assignToEmpty() {
-  return assign.apply(null, [{}].concat(Array.prototype.slice.call(arguments)));
-}
-
-export default assignToEmpty;
