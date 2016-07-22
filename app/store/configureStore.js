652c13f0f51edb2c14e4b3f732dae4cf40b3db52
Szymon Michalak <szymonmichalak@gmail.com>
2016-07-22 14:22:46 +0200

    Use redux-logger for logging redux actions

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,33 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
 
-const addLoggingToDispatch = (store) => {
-  /* eslint-disable no-console */
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
+const middleware = [thunk];
 
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-  /* eslint-enable no-console */
-};
+if (process.env.NODE_ENV !== 'production') {
+  const loggerMiddleware = createLogger();
+  middleware.push(loggerMiddleware);
+}
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 export default function configureStore(initialState) {
-  const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-  const store = createStoreWithMiddleware(rootReducer, initialState);
-
-  if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
-  }
-
-  return store;
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
