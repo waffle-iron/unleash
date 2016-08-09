0a51fd7c8400dc905112ada4daa5e2ef68295165
Szymon Michalak <szymonmichalak@gmail.com>
2016-08-09 19:55:24 +0200

    Update Skills component tests

--- app/components/__tests__/Skills.spec.js
+++ app/components/__tests__/Skills.spec.js
@@ -2,25 +2,35 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import { keyBy } from 'lodash';
+import sinon from 'sinon';
 import generate from '../../testUtils/fixtures';
 import { Skills } from '../Skills';
 
-describe('Skills List', function () {
-    let component;
-    const mockedSkills = keyBy(generate('skill', 15), 'name');
-    let mockedActions;
+describe('Skills List', () => {
+  let component;
+  const skills = generate('skill', 15);
+  const mockedSkills = keyBy(skills, 'name');
+  let mockedActions;
 
-    beforeEach(function() {
-        mockedActions = {
-            skillList: sinon.spy()
-        };
-        component = shallow(
-          <Skills skills={mockedSkills} actions={mockedActions} />
-        );
-    });
+  beforeEach(() => {
+    mockedActions = {
+      skillList: sinon.spy(),
+    };
+    const context = {
+      router: {
+        push: () => {},
+      },
+    };
+    component = shallow(<Skills skills={mockedSkills} actions={mockedActions} />, { context });
+  });
 
-    it('renders without problems', function () {
-        // expect(component).toExist();
-    });
+  it('renders without problems', () => {
+    expect(component).to.exist;
+  });
+
+  it('renders the list of skills', () => {
+    const listItems = component.find('ListItem');
+    expect(listItems.length).to.equal(skills.length);
+  });
 
 });
