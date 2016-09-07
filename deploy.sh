33e700aa547669e23cdd093a46a1f540cbe76463
karolsojko <karolsojko@gmail.com>
2016-09-07 18:47:46 +0200

    Enable deployment on develop branch

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli() {
-	aws --version
-	aws configure set default.region us-west-2
-	aws configure set default.output json
+  aws --version
+  aws configure set default.region us-west-2
+  aws configure set default.output json
 }
 
 deploy_cluster() {
@@ -22,12 +22,12 @@ deploy_cluster() {
 }
 
 make_task_def() {
-	task_template='[
-		{
-			"name": "web",
+  task_template='[
+    {
+      "name": "web",
       "image": "%s.dkr.ecr.us-west-2.amazonaws.com/x-team-unleash:%s",
-			"essential": true,
-			"memory": 300,
+      "essential": true,
+      "memory": 300,
       "logConfiguration": {
         "logDriver": "awslogs",
         "options": {
@@ -35,22 +35,22 @@ make_task_def() {
           "awslogs-region": "us-west-2"
         }
       },
-			"portMappings": [
-				{
-					"hostPort": 0,
+      "portMappings": [
+        {
+          "hostPort": 0,
           "containerPort": 3000,
           "protocol": "tcp"
-				}
-			]
-		}
-	]'
+        }
+      ]
+    }
+  ]'
 
-	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $TRAVIS_BUILD_NUMBER)
+  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $TRAVIS_BUILD_NUMBER)
 }
 
 push_ecr_image() {
-	eval $(aws ecr get-login --region us-west-2)
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/x-team-unleash:$TRAVIS_BUILD_NUMBER
+  eval $(aws ecr get-login --region us-west-2)
+  docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/x-team-unleash:$TRAVIS_BUILD_NUMBER
 }
 
 register_definition() {
